499cfd13c8fc57fde8e47e051923ab06c34083cb
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2022-06-27 00:52:46 +0530

    Add files via upload

---
+++ Wishusucess/luma_child/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Developer: Hemant Singh Magento 2X Developer
+ * Website: https://www.wishusuces.com/
+ * Catregory: How to Create Magento 2 Theme Basics
+ * 
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::THEME, 
+    'frontend/Wishusucess/luma_child', 
+    __DIR__
+);
